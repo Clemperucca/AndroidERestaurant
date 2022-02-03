05039931d1ae007532d83d41c10114de25f18631
Clemperucca <perucca.clement@gmail.com>
2022-02-03 18:32:53 +0100

    03/02/2021

--- src/main/java/fr/isen/perucca/androiderestaurant/DishesActivity.kt
+++ src/main/java/fr/isen/perucca/androiderestaurant/DishesActivity.kt
@@ -2,10 +2,25 @@ package fr.isen.perucca.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import fr.isen.perucca.androiderestaurant.databinding.ActivityDishesBinding
 
 class DishesActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityDishesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dishes)
+
+        binding = ActivityDishesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainDishTitle.text = intent.getStringExtra("category_type")
+        binding.dishRecycleView.layoutManager= LinearLayoutManager(this)
+        val data= ArrayList<String>()
+
+        for(i in 1..20){
+            data.add("Item"+ i )
+        }
+        val adapter = DishAdapter(data)
+        binding.dishRecycleView.adapter=adapter
     }
 }
